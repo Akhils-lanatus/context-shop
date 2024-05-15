67f3359d7bc43d47f5f1d7c659be819d7e90a4d0
Akhils-lanatus <akhil.s@lanatussystems.com>
2024-05-15 15:53:03 +0530

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,27 @@
+import React, { useState, useEffect } from "react";
+
 const App = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      await fetch("https://example-data.draftbit.com/products")
+        .then((response) => response.json())
+        .then((response) => setData(response));
+    };
+    getData();
+  }, []);
+
+  const categoryCounts = {};
+
+  data.forEach((item) => {
+    const category = item.category;
+    categoryCounts[category] = (categoryCounts[category] || 0) + 1;
+  });
+
+  const sortedData = Object.fromEntries(
+    Object.entries(categoryCounts).sort(([, a], [, b]) => b - a)
+  );
+
   return <div>App</div>;
 };
 
